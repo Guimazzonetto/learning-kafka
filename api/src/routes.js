86f0116de13ac7aaa4cd037e0fa68f2044f6c222
Guilherme Brito <guilherme.brito@claravista.com.br>
2023-12-14 11:39:14 -0300

    feat: created docker-compose.yml

--- api/src/routes.js
+++ api/src/routes.js
@@ -3,16 +3,19 @@ import express from "express";
 const routes = express.Router();
 
 routes.post("/certifications", async (req, res) => {
+  const message = {
+    user: {
+      id: 1,
+      name: "Diego Fernandes",
+    },
+    course: "Kafka com Node.js",
+    grade: 5,
+  };
+
   // Chamar microserviço
-  await producer.send({
+  await req.producer.send({
     topic: "issue-certificate",
-    messages: [
-      {
-        user: { id: 1, name: "Diego Fernandes" },
-        course: "Kafka com Node.js",
-        grade: 5,
-      },
-    ],
+    messages: [{ value: JSON.stringify(message) }],
   });
 
   console.log(req.producer);
